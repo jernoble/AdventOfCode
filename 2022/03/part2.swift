a67868aa976359c7329d10fc96432a1a02926ead
Jer Noble <jer.noble@apple.com>
2022-12-03 14:52:12 -0800

    Slight performance improvement in 2022/03/part2.swift

--- 2022/03/part2.swift
+++ 2022/03/part2.swift
@@ -5,6 +5,11 @@ struct Rucksack {
         if input == nil { return nil }
         let stringArray = Array(input!.utf8)
         var items: UInt64 = 0
+
+        // Get the "value" of each input character, which will be a number from 1 -> 52.
+        // Then, "store" this value by setting the Nth bit in a 64-bit integer, where N
+        // is equal to the character's "value". This allows each value to appear multiple
+        // times in the input string, and only costs 8-bytes to store.
         for character in stringArray {
             let itemBitPosition = Rucksack.valueOf(character)
             items |= 1 << itemBitPosition
@@ -15,11 +20,9 @@ struct Rucksack {
     static let firstLowerCharacter: UInt8 = Character("a").asciiValue!
     static let firstUpperCharacter: UInt8 = Character("A").asciiValue!
 
-    static func valueOf(_ item: UInt8?) -> Int {
-        if item == nil { return 0 }
-        let itemValue = item!
-        if itemValue >= firstLowerCharacter { return Int(itemValue - firstLowerCharacter + 1) }
-        else { return Int(itemValue - firstUpperCharacter + 27) }
+    static func valueOf(_ item: UInt8) -> Int {
+        if item >= firstLowerCharacter { return Int(item - firstLowerCharacter + 1) }
+        else { return Int(item - firstUpperCharacter + 27) }
     }
 }
 
@@ -27,6 +30,11 @@ struct Group {
     let rucksacks: [Rucksack]
 
     func score() -> Int {
+        // Storing each Rucksack's `items` as a bitfield makes comparisons between as cheap
+        // as bitwise-and'ing the `items` against each other, and extracting the number
+        // of trailing zeros from the result.
+        // NOTE: this does not validate that there is exactly one "item" in the result.
+        // In the case of invalid input, this will return a "score" of 64.
         if rucksacks.isEmpty { return 0 }
         var itemsBitfield = rucksacks[0].itemsBitfield
         for rucksack in rucksacks[1...] {
